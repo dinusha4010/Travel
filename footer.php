4cca71c2342f466311ae1a539801266dad661ec4
KaluarachchiKCD <dinusha4010@gmail.com>
2024-02-06 01:52:36 +0200

    footer changed

--- footer.php
+++ footer.php
@@ -1,43 +1,40 @@
 <footer>
+<div class="container text-center">    
+    <div class="row justify-content-md-center ">
+    
         
-        <div class="row justify-content-md-center">
-        <div class="col-md-3">
-          <div class="card">
-            <img src="images/frame1/logo.gif" alt="image"  class="rounded mx-auto d-block img-fluid" width="75" height="100">  
+    
+  
+    <div class="col-md-3">
+    <img src="images/frame1/logo.gif" alt="image"  class="rounded mx-auto d-block img-fluid" width="75" height="100">  
             <p style="text-align: center;"><br>Tourism Hotline<br>1912 <br><br><br><br><br></p>
-          </div>
-          </div>
-          <div class="col-md-3">
-          <div class="card">
-          <p style="text-align: center;">Quick Links</p>
-          <a href="">Subscribe</a>
-          <a href="">Contact Us          </a>
-          <a href="">Important Notices          </a>
-          <a href="">Terms of Use</a>
-          <a href="">Complaints</a>
+    </div>
+    <div class="col-md-3">
+    <a href="">Subscribe<br></a>
+          <a href="">Contact Us<br>        </a>
+          <a href="">Important Notices<br>          </a>
+          <a href="">Terms of Use<br></a>
+          <a href="">Complaints<br></a>
           <p style="text-align: center;"><br><br>&copy; 2024 All Rights Reserved by Team 12</p>
           
-          </div>
-          </div>
-          <div class="col-md-3"> 
-          <div class="card">
-            <p style="text-align: center;">Follow Us On</p>   
+    </div>
+    <div class="col-md-3 ">
+    <p style="text-align: center;">Follow Us On</p>   
            <a href=""> <img src="images/frame3/facebook.png" alt="image"  width="50" height="50" style="padding: 5px;"></a>
            <a href=""> <img src="images/frame3/instagram.png" alt="image" width="50" height="50" style="padding: 5px;"></a>
            <a href=""> <img src="images/frame3/twitter.png" alt="image" width="50" height="50" style="padding: 5px;"></a>
            <a href=""> <img src="images/frame3/whatsapp.png" alt="image" width="50" height="50" style="padding: 5px;"></a>
-
-          </div>    
-          </div>
-       
+    </div>
+  </div>
+</div>
       
       
           
         </div>
-        
+</div>
 
       </footer>
-  </div>   
+ 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
